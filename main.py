5b87c3bfc8e773eb426a2472042b5380d6aa42bc
vapecatcoin <163068392+vapecatcoin@users.noreply.github.com>
2024-03-12 17:45:37 +0000

    Init telebot

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+import telebot
+
+# Accessing GitHub Actions context
+DEV_CHAT_ID = os.environ.get('DEV_CHAT_ID') or 'unknown'
+repository = os.environ.get('GITHUB_REPOSITORY') or 'unknown'
+event_name = os.environ.get('GITHUB_EVENT_NAME') or 'unknown'
+branch = os.environ.get('GITHUB_REF') or 'unknown'
+actor = os.environ.get('GITHUB_ACTOR') or 'unknown'
+repository_owner = os.environ.get('GITHUB_REPOSITORY_OWNER') or 'unknown'
+my_secret = os.environ.get('MY_SECRET')
+
+bot = telebot.TeleBot(my_secret)
+
+# build repo url
+repo_url = f"https://www.github.com/{repository}"
+
+# formatted_message = f"New {event_name} on {repository} by {actor} on branch {branch}"
+formatted_message = f"<b>New Event on GitHub</b>" \
+                    f"\n\n" \
+                    f"Repository: {repository}" \
+                    f"\nEvent name: {event_name}" \
+                    f"\nBranch: {branch}" \
+                    f"\nActor: {actor}" \
+                    f"\nRepository owner: {repository_owner}" \
+                    f"\n\n<a href='{repo_url}'>{repository}</a>\nMirrored to Arweave" \
+                    
+bot.send_message(DEV_CHAT_ID, formatted_message, parse_mode='HTML')
