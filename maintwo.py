70e3ea5ecfdae8e6101dd4632164bd0b127473cd
vapecatcoin <163068392+vapecatcoin@users.noreply.github.com>
2024-03-19 11:13:21 +0000

    update

--- maintwo.py
+++ maintwo.py
@@ -14,6 +14,8 @@ commit_url = os.environ.get('COMMIT_URL') or 'unknown'
 
 bot = telebot.TeleBot(my_secret)
 
+MAIN_GIF = "https://raw.githubusercontent.com/vapecatcoin/cdn/main/catrings.gif"
+
 def send_formatted_message(url):
     formatted_message = f"<b>New Event on GitHub</b>" \
                     f"\n\n" \
@@ -24,6 +26,17 @@ def send_formatted_message(url):
                     f"\nRepository owner: {repository_owner}" \
                     f"\n\n<a href='{url}'>Check it out!</a>\n"
     bot.send_message(DEV_CHAT_ID, formatted_message, parse_mode='HTML', disable_web_page_preview=True)
+
+def send_formatted_message_with_gif(url, gif_url):
+    formatted_message = f"<b>New Event on GitHub</b>" \
+                    f"\n\n" \
+                    f"Repository: {repository}" \
+                    f"\nEvent name: {event_name}" \
+                    f"\nBranch: {branch}" \
+                    f"\nActor: {actor}" \
+                    f"\nRepository owner: {repository_owner}" \
+                    f"\n\n<a href='{url}'>Check it out!</a>\n"
+    bot.send_photo(DEV_CHAT_ID, gif_url, caption=formatted_message, parse_mode='HTML', supports_streaming=True)
 
 def main():
     """
@@ -38,14 +51,14 @@ def main():
         github_pr_url = f"https://www.github.com/{repository}/pull/{args.pr_number}"
         print(github_pr_url)
 
-        send_formatted_message(github_pr_url)
+        send_formatted_message_with_gif(github_pr_url, MAIN_GIF)
 
     else:
         print("This is not a pull request")
         # make commit url for the push event
         if event_name == 'push':
             print(commit_url)
-            send_formatted_message(commit_url)
+            send_formatted_message_with_gif(github_pr_url, MAIN_GIF)
         
 if __name__ == "__main__":
     main()
